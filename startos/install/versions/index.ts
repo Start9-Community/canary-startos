2141d4651cb90f6c7831c41b6384dacf76dccfdf
Matt Hill <mattnine@protonmail.com>
2026-03-19 18:51:02 -0600

    bump version

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,3 +1,2 @@
-import { v_1_3_1_0_b0 } from './v1.3.1.0.b0'
-export { v_1_4_0_0_b2 as current } from './v1_4_0.0.b2'
-export const other = [v_1_3_1_0_b0]
+export { v_1_4_0_1_b0 as current } from './v1_4_0_1_b0'
+export const other = []
